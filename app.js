e4b7b1ad43641548d77c3c1662dd7a317ba338ff
maggpro <maggpro@gmail.com>
2024-12-02 17:09:31 +0300

    Update game files

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-updateUI() {
-    const userStats = document.querySelector('.user-stats');
-    if (userStats) {
-        userStats.innerHTML = `
-            <h2>Ваш счет: ${this.points} influencer</h2>
-            <p>Звезды: ${this.stars} ⭐️</p>
-        `;
-    }
-}
+// Конфигурация Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyD37QLRUg3rsT5upQddh7JquYsAjDb35Pk",
+    authDomain: "testwf-cd5ad.firebaseapp.com",
+    projectId: "testwf-cd5ad",
+    storageBucket: "testwf-cd5ad.appspot.com",
+    messagingSenderId: "68921357629",
+    appId: "1:68921357629:web:xxxxxxxxxxxxxx"
+};
+// ... остальной код app.js ...
